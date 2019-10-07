392e9d45e11ad2c94d20be6586bf20689c9efdb7
Espen Norderud <espen42@gmail.com>
2019-10-07 23:19:56 +0200

    Added the first minimal example: hello-react

--- src/main/resources/site/REPLACE_ME.jsx
+++
@@ -1,6 +0,0 @@
-/* Webpack won't build if there's not at least one JSX entry below the site folder.
-   This placeholder takes care of that.
-   Remove me when you add your own React components below site - primarily below /parts or /pages.
- */
-
-export default {};

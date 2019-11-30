bd19131db08f5b7078716173873372df26bd456b
Espen Norderud <espen42@gmail.com>
2019-11-30 19:44:17 +0100

    Better comments, leaner default build logging and resulting files.

--- webpack.config.react4xp.js
+++ webpack.config.react4xp.js
@@ -1,6 +1,4 @@
 module.exports = function(env, config) {
-    console.log(JSON.stringify(config, null, 4));
-
     // This makes 'npm link' symlinks in node_modules work:
     config.resolve.symlinks = false;
 

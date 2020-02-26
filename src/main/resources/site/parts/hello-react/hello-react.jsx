05120d3b13e1052b4a2db4d4536287d6b204052b
Espen Norderud <espen42@gmail.com>
2020-02-26 09:40:31 +0100

    Working on diagnosing the bug: lib-react issue #35.

--- src/main/resources/site/parts/hello-react/hello-react.jsx
+++ src/main/resources/site/parts/hello-react/hello-react.jsx
@@ -1,3 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default (props) => <p>Hello {props.greetee}!</p>;
+function Hey(props) {
+    const [count, setCount] = useState(1);
+    const message = ` Hello ${props.greetee}`;
+    return <p onClick={()=>setCount(count + 1)}>{message.repeat(count)}!</p>;
+};
+export default Hey;

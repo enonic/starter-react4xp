b7b4731b183d7d089f0102309bea1fe81ccced97
Espen Norderud <espen42@gmail.com>
2020-04-03 13:01:15 +0200

    Starting cleanup before release 1.0

--- src/main/resources/site/parts/hello-react/hello-react.jsx
+++ src/main/resources/site/parts/hello-react/hello-react.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 
-function Hey(props) {
+require('react-dom');
+console.log("hello-react - window.React1:", window.React1);
+window.React2 = require('react');
+console.log("hello-react - window.React2:", window.React2);
+console.log("hello-react - window.React3:", window.React3);
+console.log("hello-react - equal:        ", window.React1 === window.React2);
+console.log("hello-react - equal 2/3     ", window.React3 === window.React2);
+
+export default (props) => {
     const [count, setCount] = useState(1);
     const message = ` Hello ${props.greetee}`;
     return <p onClick={()=>setCount(count + 1)}>{message.repeat(count)}!</p>;
 };
-export default Hey;

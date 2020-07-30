ccc14cb8fa15b1e4ab45e285b96ba1a9e86edf5b
Espen Norderud <espen42@gmail.com>
2020-07-30 13:33:18 +0200

    Revert "Added multi-color example"
    This reverts commit 5afc8ad4764d5cc1a6879f19463dd281702e4610.

--- webpack.config.react4xp.js
+++ webpack.config.react4xp.js
@@ -1,47 +1,12 @@
 /** Use this file to adjust the webpack config.
  *  Uncomment the overrideComponentWebpack property in react4xp.properties, and add this file there.
  */
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = function(env, config) {
 
     // Makes symlinks under node_modules work, e.g. 'npm link' and possibly PNPM etc:
     config.resolve.symlinks = true;
-
-    // Compile .scss and friends:
-    config.module.rules = [
-        ...(config.module.rules || []),
-        {
-            test: /\.((sa|sc|c))ss$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 1,
-                        modules: { auto: true },
-                    }
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sassOptions: {
-                            outputStyle: 'compressed',
-                        },
-                    },
-                }
-            ]
-        }
-    ];
-
-    // Set up how the compiled assets are exported:
-    config.plugins = [
-        ...(config.plugins || []),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].[contenthash:9].css',
-        }),
-    ];
 
     return config;
 };

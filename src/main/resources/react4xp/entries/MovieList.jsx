17fd5ee22b61b26ef62580fa3121f1fdb24adae4
Espen Norderud <espen42@gmail.com>
2020-09-17 16:19:58 +0200

    4th-to-last: remove basic framework for interactivity (onclick, the clicked makeRequest function, the init stage and necessary variables)

--- src/main/resources/react4xp/entries/MovieList.jsx
+++ src/main/resources/react4xp/entries/MovieList.jsx
@@ -4,38 +4,10 @@ import './MovieList.scss';
 
 import Movie from "./Movie";
 
-// State values that don't need re-rendering capability, but need to be synchronously read/writable across closures.
-let isInitialized = false;
-let nextOffset = 0;             // Index for what will be the next movie to search for in a guillotine request
-
-
-const MovieList = ({movies, apiUrl, parentId, movieCount, movieType, sortExpression}) => {
-
-    if (!isInitialized) {
-        isInitialized = true;
-
-        nextOffset = movieCount;
-    }
-
-    // ------------------------------------------------------
-    // Set up action methods, triggered by listener:
-
-    // Makes a (guillotine) request for data with these search parameters and passes an anonymous callback function as
-    // handleDataFunc (used on the returned list of movie data).
-    const makeRequest = () => {
-        console.log("Requesting", movieCount, "movies, starting from index", nextOffset);
-    };
-
-
-    // ------------------------------------------------------------------------------------
-    // Actual rendering:
-
-
-    console.log("------------------------- Rendering movies:", movies.map(movie => movie.title));
-    console.log("Click to add more movies, starting at index", nextOffset);
+const MovieList = ({movies, parentId, apiUrl, movieCount, movieType, sortExpression}) => {
 
     return (
-        <div id={`movieList_${parentId}`} className="movieList" onClick={makeRequest}>
+        <div id={`movieList_${parentId}`} className="movieList">
             {movies
                 ? movies.map(movie =>
                         <Movie key={movie.id} {...movie} />

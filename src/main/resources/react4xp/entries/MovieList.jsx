57b8e978275cdef93787d32b6c6a713687a86278
Espen Norderud <espen42@gmail.com>
2020-09-22 01:43:44 +0200

    Result: a (styled) MovieList entry, nesting a list of Movie components it gets through props	52747f6	Espen Norderud <espen42@gmail.com>	22 Sep 2020 at 00:30
    This reverts commit a150a88666aa4e5f6d552286fdea173b7a6668da.

---
+++ src/main/resources/react4xp/entries/MovieList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './MovieList.scss';
+
+import Movie from "../shared/movie/Movie";
+
+const MovieList = ({movies}) => {
+
+    console.log("------------------------- Rendering movies:", movies.map(movie => movie.title));
+
+    return (
+        <div className="movieList">
+            {movies
+                ? movies.map(movie =>
+                        <Movie key={movie.id} {...movie} />
+                    )
+                : null
+            }
+        </div>
+    );
+};
+
+// MUST use this export line wrapping, because of the useState hook.
+export default (props) => <MovieList {...props} />;

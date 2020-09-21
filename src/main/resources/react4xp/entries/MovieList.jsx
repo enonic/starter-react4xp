3a542f623dab139a0a2d4926e34d4b6eef4fc853
Espen Norderud <espen42@gmail.com>
2020-09-21 22:46:15 +0200

    Result: onClick method makes a guillotine request and console-logs the returned result
    (removed state hook and container ID)

--- src/main/resources/react4xp/entries/MovieList.jsx
+++ src/main/resources/react4xp/entries/MovieList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import './MovieList.scss';
 
@@ -14,14 +14,6 @@ let nextOffset = 0;             // Index for what will be the next movie to sear
 
 
 const MovieList = ({movies, apiUrl, parentPath, movieCount, movieType, sortExpression}) => {
-
-    // Setup asunchronous component state that triggers re-render on change.
-    const [state, setState] = useState({
-        movies,                     // Array of data objects: currently displayed movies
-    });
-
-    const listContainerId = `movieListContainer_${parentPath}`;
-
     if (!isInitialized) {
         isInitialized = true;
 
@@ -56,8 +48,6 @@ const MovieList = ({movies, apiUrl, parentPath, movieCount, movieType, sortExpre
 
 
     // When a movie data array is returned from the guillotine data request, this method is called.
-    // Merges incoming movie data into the component state (which react automatically renders to the DOM), preventing duplicates.
-    // Also updates the index in the movies data that the next item should start searching at.
     const updateDOMWithNewMovies = (newMovieItems) => {
         console.log("Received data:", newMovieItems);
         console.log(newMovieItems.map(movie => movie.title));
@@ -70,13 +60,13 @@ const MovieList = ({movies, apiUrl, parentPath, movieCount, movieType, sortExpre
     // Actual rendering:
 
 
-    console.log("------------------------- Rendering state.movies:", state.movies.map(movie => movie.title));
+    console.log("------------------------- Rendering movies:", movies.map(movie => movie.title));
     console.log("Click to add more movies, starting at index", nextOffset);
 
     return (
-        <div id={`${listContainerId}`} className="movieList" onClick={makeRequest}>
-            {state.movies
-                ? state.movies.map(movie =>
+        <div className="movieList" onClick={makeRequest}>
+            {movies
+                ? movies.map(movie =>
                         <Movie key={movie.id} {...movie} />
                     )
                 : null

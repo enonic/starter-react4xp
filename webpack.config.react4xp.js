642001eec6a3b83e930ebef477b363badb62cdc6
Espen Norderud <espen42@gmail.com>
2020-09-22 01:14:42 +0200

    Result: ready to start. First step:
    npm i -D css-loader@3 mini-css-extract-plugin node-sass@4 sass-loader@8
    
    (reset webpack.config.react4xp.js and react4xp.properties)

--- webpack.config.react4xp.js
+++ webpack.config.react4xp.js
@@ -1,44 +1,12 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+/** Use this file to adjust the webpack config.
+ *  Uncomment the overrideComponentWebpack property in react4xp.properties, and add this file there.
+ */
+
 
 module.exports = function(env, config) {
 
-    // This makes 'npm link' symlinks in node_modules work:
+    // Makes symlinks under node_modules work, e.g. 'npm link' and possibly PNPM etc:
     config.resolve.symlinks = true;
-
-    // Compile .scss and friends:
-    config.module.rules = [
-        ...(config.module.rules || []),
-        {
-            test: /\.((sa|sc|c))ss$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 1,
-                        modules: { auto: true },
-                    }
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sassOptions: {
-                            outputStyle: 'compressed',
-                        },
-                    },
-                }
-            ]
-        }
-    ];
-
-    // Set up how the compiled assets are exported:
-    config.plugins = [
-        ...(config.plugins || []),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].[contenthash:9].css',
-        }),
-    ];
 
     return config;
 };

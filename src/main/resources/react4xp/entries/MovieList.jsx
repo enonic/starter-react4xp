786b7e5c35127ae9348ddabae3e30c9ea30e3661
Espen Norderud <espen42@gmail.com>
2020-09-22 02:42:57 +0200

    Result: adding a simple click listener to the MovieList entry, and starting an initialization stage

--- src/main/resources/react4xp/entries/MovieList.jsx
+++ src/main/resources/react4xp/entries/MovieList.jsx
@@ -4,12 +4,37 @@ import './MovieList.scss';
 
 import Movie from "../shared/movie/Movie";
 
+// State values that don't need re-rendering capability, but need to be synchronously read/writable across closures.
+let isInitialized = false;
+let nextOffset = 0;             // Index for what will be the next movie to search for in a guillotine request
+
+
 const MovieList = ({movies}) => {
+    if (!isInitialized) {
+        isInitialized = true;
+
+        nextOffset = movies.length;
+    }
+
+    // ------------------------------------------------------
+    // Set up action methods, triggered by listener:
+
+    // Makes a (guillotine) request for data with these search parameters and passes an anonymous callback function as
+    // handleDataFunc (used on the returned list of movie data).
+    const makeRequest = () => {
+        console.log("Requesting", movies.length, "movies, starting from index", nextOffset);
+
+        nextOffset += movies.length;
+    };
+
+    // ------------------------------------------------------------------------------------
+    // Actual rendering:
 
     console.log("------------------------- Rendering movies:", movies.map(movie => movie.title));
+    console.log("Click to add more movies, starting at index", nextOffset);
 
     return (
-        <div className="movieList">
+        <div className="movieList" onClick={makeRequest}>
             {movies
                 ? movies.map(movie =>
                         <Movie key={movie.id} {...movie} />

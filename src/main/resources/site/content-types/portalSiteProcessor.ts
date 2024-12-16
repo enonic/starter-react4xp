df2a1069e002e86177f10ed1f805788885379656
Christian Westgaard <comlock@gmail.com>
2024-12-16 14:39:26 +0100

    Site displayName

--- src/main/resources/site/content-types/portalSiteProcessor.ts
+++ src/main/resources/site/content-types/portalSiteProcessor.ts
@@ -2,15 +2,20 @@ import type { Site } from '@enonic-types/lib-content';
 import type { ContentTypeProcessorFunction } from '/lib/enonic/react4xp/DataFetcher';
 // import type { PortalSiteProps } from '/types/PortalSiteProps';
 
-// import { toStr } from '@enonic/js-utils/value/toStr';
+import { toStr } from '@enonic/js-utils/value/toStr';
 
 export const portalSiteProcessor: ContentTypeProcessorFunction<
 	Site<Record<string, unknown>>
 > = (params) => {
-	// log.info('portalSiteProcessor params:%s', toStr(params));
+	log.info('portalSiteProcessor params:%s', toStr(params));
+	const {
+		content: {
+			displayName: siteDisplayName
+		}
+	} = params;
 	return {
 		props: /*<PortalSiteProps>*/{
-			title: 'React4XP Starter',
+			title: `React4XP Starter: ${siteDisplayName}`,
 			text: 'Welcome to the React4XP starter!',
 		}
 	};

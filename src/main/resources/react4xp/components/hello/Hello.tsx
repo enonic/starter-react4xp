c8b6164f47bd46110fb9e5cb2e3b20964e4a39d4
Mathias Skjulestad <msk@enonic.com>
2025-06-24 10:22:53 +0200

    More neutral design #1386
    Update hello component to a more neutral design as a more personal design is to be implemented through global styles later in tutorial.

--- src/main/resources/react4xp/components/hello/Hello.tsx
+++ src/main/resources/react4xp/components/hello/Hello.tsx
@@ -6,11 +6,9 @@ export const Hello = ({title, initialCount}: HelloProps) => {
     const [count, setCount] = React.useState(initialCount);
 
     return (
-        <div>
-            <section className={styles.helloContainer}>
+        <section className={`helloPage ${styles.helloContainer}`}>
                 <h1>{title}</h1>
                 <button onClick={() => setCount(count + 1)}>{count} Clicks!</button>
             </section>
-        </div>
     );
 };

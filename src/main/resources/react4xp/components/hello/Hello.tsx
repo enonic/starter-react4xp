72480ab83b789068764721a2bd3bb1927f5dd2ec
Mathias Skjulestad <msk@enonic.com>
2025-07-10 10:38:56 +0200

    #1398 Prepare and release new version of starter-react4xp (#1399)
    * #1398 Prepare and release new version of starter-react4xp
    
    Update react components and lib-react4xp to RC3
    Patch Hello component and realign app.tsx, app.ts and remove HelloProps
    
    * Update src/main/resources/react4xp/components/hello/Hello.tsx
    
    Co-authored-by: Pavel Milkevich <pmi@enonic.com>
    
    ---------
    
    Co-authored-by: Pavel Milkevich <pmi@enonic.com>

--- src/main/resources/react4xp/components/hello/Hello.tsx
+++ src/main/resources/react4xp/components/hello/Hello.tsx
@@ -1,14 +1,20 @@
-import type {HelloProps} from '/types/HelloProps';
+import type {ComponentProps} from '@enonic/react-components';
 import * as React from 'react';
 import styles from './Hello.module.css';
 
-export const Hello = ({title, initialCount}: HelloProps) => {
-    const [count, setCount] = React.useState(initialCount);
+export interface HelloProps
+    extends Record<string, unknown> {
+    title: string
+    initialCount: number
+}
 
+export const Hello = (props: ComponentProps) => {
+    const {title, initialCount} = props.data as HelloProps;
+    const [count, setCount] = React.useState(initialCount);
     return (
         <section className={`helloPage ${styles.helloContainer}`}>
-                <h1>{title}</h1>
-                <button onClick={() => setCount(count + 1)}>{count} Clicks!</button>
-            </section>
+            <h1>{title}</h1>
+            <button onClick={() => setCount(count + 1)}>{count} Clicks!</button>
+        </section>
     );
 };
--- src/main/resources/types/HelloProps.ts
+++
@@ -1,4 +0,0 @@
-export interface HelloProps {
-    title: string
-    initialCount: number
-}
